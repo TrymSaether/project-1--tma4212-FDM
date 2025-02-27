78071c2f12dcf58952c3b7c71f699acfdcc9c6ce
Ludde <157131052+tlhaugen@users.noreply.github.com>
2025-02-27 20:24:40 +0100

    Added beta function

--- simulation/SIRSimulation.py
+++ simulation/SIRSimulation.py
@@ -4,8 +4,8 @@ from matplotlib.animation import FuncAnimation
 from scipy.sparse import diags, eye, kron
 
 class SIRSimulation:
-    def __init__(self, n=0, L=1.0, M=50, T=10, dt=0.001, initial_infection=0.01,
-                 beta=3, gamma=1, mu_s=0.001, mu_i=0.001):
+    def __init__(self, L=1.0, M=50, T=10, dt=0.001, initial_infection=0.01,
+                 beta=3, gamma=1, mu_s=0.001, mu_i=0.001, n=0, dynamic_beta=False):
         self.L = L
         self.M = M
         self.h = L / M
@@ -18,6 +18,7 @@ class SIRSimulation:
         self.mu_i = mu_i
         self.ii = initial_infection
         self.n = n
+        self.db = dynamic_beta
 
         self.x = np.linspace(0, L, M)
         self.X, self.Y = np.meshgrid(self.x, self.x)
@@ -32,19 +33,19 @@ class SIRSimulation:
         self.L_matrix = self.laplacian()
     
     def initial_condition(self):
-        if self.n == 0:  # 3 random infection areas
-            cx, cy = np.random.uniform(0.1, 0.9, 3) * self.L, np.random.uniform(0.1, 0.9, 3) * self.L
+        if self.n == 0:  #Dense infection in corners
+            dist_to_corner = (self.X - self.L / 4)**2 + (self.Y - self.L / 4)**2
+            infection = (self.ii * np.exp(-200 * dist_to_corner)).ravel()
+            self.I = infection + infection[::-1]
+            
+        elif self.n == 1:  #Linear front of infection
+            self.I[self.X.ravel() < self.L * 0.2] = self.ii
+
+        else:  #3 random infection areas
+            cx, cy = np.random.uniform(0.3, 0.7, 3) * self.L, np.random.uniform(0.1, 0.9, 3) * self.L
             X_flat, Y_flat = self.X.ravel(), self.Y.ravel()
             distances = np.min((X_flat[:, None] - cx) ** 2 + (Y_flat[:, None] - cy) ** 2, axis=1)
             self.I[distances < 0.009] = self.ii
-
-        elif self.n == 1:  # Linear front of infection
-            self.I[self.X.ravel() < self.L * 0.2] = self.ii
-
-        else:  # Dense central infection
-            dist_to_center = (self.X - self.L / 2) ** 2 + (self.Y - self.L / 2) ** 2
-            self.I = self.ii * np.exp(-100 * dist_to_center)
-
 
 
 
@@ -58,10 +59,26 @@ class SIRSimulation:
         
         return kron(I, L) + kron(L, I)
 
-    
+    def beta_function(self, t):
+        dist_to_center = (self.X - self.L / 2)**2 + (self.Y - self.L / 2)**2
+        spatial_factor = 1 + np.exp(-10 * dist_to_center)  # Higher in center
 
-    def step(self):
-        infection = self.beta * self.S * self.I
+        if 2 <= t <= 2+np.pi:
+            time_factor = 1 + np.sin(t-2.5)  # Fluctuates with time
+        else:
+            time_factor = 0
+
+        return self.beta * spatial_factor * time_factor #* moving_hotspot
+
+
+    def step(self, t):
+        if self.db:
+            beta = self.beta_function(t).ravel()
+            print(f'{beta.shape}')
+        else:
+            beta = self.beta
+
+        infection = beta * self.S * self.I
         recovery = self.gamma * self.I
 
         diffusion_S = self.mu_s * self.L_matrix.dot(self.S)
@@ -87,7 +104,7 @@ class SIRSimulation:
 
         def update(frame):
             for _ in range(50): 
-                self.step()
+                self.step(frame * self.dt * 50)
             ax.clear()
             ax.plot_surface(self.X, self.Y, self.I.reshape(self.M, self.M), cmap='hot', edgecolor='none')
             ax.set_zlim(0, 1)
@@ -100,5 +117,6 @@ class SIRSimulation:
         plt.show()
 
 
-sim = SIRSimulation()
+sim = SIRSimulation(dynamic_beta=True)
 sim.show_initial()
+sim.animate()
